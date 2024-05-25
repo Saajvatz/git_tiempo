0461964d1842d219b0192dd5b64466bbb01aa863
pagiron <pagiron@scl.edu.gt>
2024-05-24 20:42:08 -0600

    Footer y Header editados

---
+++ footer/footer.js
@@ -0,0 +1,6 @@
+let div_footer = document.querySelector(".footer");
+
+div_footer.innerHTML = `
+  <div class="footer12">
+    <h1>Santa Catalina Laboure</h1>
+  </div>`;
---
+++ header/header.js
@@ -0,0 +1,6 @@
+let div_header = document.querySelector(".header");
+
+div_header.innerHTML = `
+  <div class="header12">
+    <h1>Hola Mundo</h1>
+  </div>`;
